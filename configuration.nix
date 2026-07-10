d6c70ea40d833878a78e445ccb4ca1a48cc4fe5f
lee <sleechengn@live.cn>
2026-07-10 10:17:31 +0800

    保存homem

--- configuration.nix
+++ configuration.nix
@@ -155,6 +155,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     tmux
+    qq
     gnome-tweaks
     microsoft-edge
     fastfetch
